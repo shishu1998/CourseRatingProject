86b4404630827e8173580ffab2b8b5dbf22c58b5
shishu1998 <shishu_1998@yahoo.com>
2018-03-10 23:01:25 -0500

    Added a stored procedure and function for Users

---
+++ UserProcedures.sql
@@ -0,0 +1,25 @@
+-- Validates a User with the given Username and Password, returns a boolean
+DELIMITER $$
+CREATE FUNCTION ValidateUser(UserN VARCHAR(20), PASS VARCHAR(64))
+	RETURNS BOOLEAN
+ BEGIN
+	DECLARE validated boolean;
+    DECLARE pWord VARCHAR(64);
+    SET pWord = (SELECT LoginPassword FROM Users WHERE UserName = UserN);
+ 	IF (pWord IS NULL) THEN SET validated = false;
+    ELSEIF (pWord = PASS)
+		THEN SET validated = true;
+	ELSE
+		SET validated = false;
+	END IF;
+    Return validated;
+ END $$
+DELIMITER ;
+
+-- Returns the Courses that a User takes based on the UserID provided
+DELIMITER //
+CREATE PROCEDURE GetCoursesByUser(IN ID int)
+ BEGIN
+	SELECT * FROM InCourse WHERE UserID = ID;
+ END //
+DELIMITER ;
