5fd866a485a546cdba9f7aa5e0924f8dc32e9f1f
shishu1998 <shishu_1998@yahoo.com>
2018-04-05 21:02:54 -0400

    added stored procedures for adding ratings and registering

--- UserProcedures.sql
+++ UserProcedures.sql
@@ -1,5 +1,7 @@
 DROP PROCEDURE IF EXISTS GetCoursesByUser;
 DROP PROCEDURE IF EXISTS GetCOursesByUserName;
+DROP PROCEDURE IF EXISTS RegisterStudent;
+DROP PROCEDURE IF EXISTS AddRating;
 
 -- Returns the Courses that a User takes based on the UserID provided
 DELIMITER //
@@ -17,4 +19,27 @@ CREATE PROCEDURE GetCoursesByUsername(IN ID VARCHAR(20))
 	SET IntID = (SELECT UserID FROM Users WHERE UserName = ID);
     CALL GetCoursesByUser(IntID);
  END //
+DELIMITER ;
+
+-- Creates a new Student with the given ID and name, returns true or false depends on if user already exists
+DELIMITER //
+CREATE PROCEDURE RegisterStudent(IN UName VARCHAR(20), IN PW VARCHAR(64), IN FNAME VARCHAR(64), OUT SUCCESS Boolean)
+BEGIN
+	IF UNAME IN (SELECT UserName FROM Users)
+		THEN SET SUCCESS = false;
+	ELSE
+		BEGIN
+			INSERT INTO Users (UserName, LoginPassword, FullName, UserTypeID) VALUES (UName, PW, FNAME, 1);
+            SET SUCCESS = true;
+		END;
+    END IF;
+END //
+DELIMITER ;
+
+-- Creates a new rating in the rating page
+DELIMITER //
+CREATE PROCEDURE AddRating(IN CourseCode VARCHAR(10), IN Sem INT, IN Section VARCHAR(2), IN Rate VARCHAR(10), IN NOTE VARCHAR(150))
+BEGIN
+	INSERT INTO Rating (CourseID, SemesterID, SectionName, Rating, Notes) VALUES (CourseCode, Sem, Section, Rate, Note);
+END //
 DELIMITER ;
