8c2e247a8e8f91d3826bf35524f5dd8dad4f135f
shishu1998 <shishu_1998@yahoo.com>
2018-04-08 16:20:24 -0400

    Login page

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from utils import *
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    error = None
+    if request.method == 'POST':
+        if ValidateUser(request.form['UserName'], request.form['Password']):
+            return redirect(url_for('rate', UserName=request.form['UserName']))
+        else:
+            error = 'Invalid Credentials. Please try again.'
+    return render_template('index.html', error=error)
+
+@app.route('/rate')
+def rate():
+    return render_template('rate.html',UserName=request.args['UserName'])
